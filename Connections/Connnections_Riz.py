ff7fd9db86cc48e4e2fe59f3214d4f04143b40a4
ShaikRizwana-lab <x22114611@student.ncirl.ie>
2023-04-18 16:30:28 +0100

    first push

---
+++ Connections/Connnections_Riz.py
@@ -0,0 +1,44 @@
+import pymongo
+from neo4j import GraphDatabase
+from neo4j.exceptions import ServiceUnavailable
+import mysql.connector
+from mysql.connector.errors import OperationalError
+import time
+import urllib
+
+class DBConnections:
+    def Connection_mongo():
+        try:
+            client = pymongo.MongoClient('mongodb://localhost:27017'
+                     #username='user',
+                     #password='password',
+                     #authSource='the_database',
+                     #authMechanism='SCRAM-SHA-256'
+                     ) 
+            #mongo_uri = "mongodb://dap2023:" + urllib.parse.quote("password@123") + "@cluster0.gbbogr4.mongodb.net"
+            #client2 = pymongo.MongoClient(mongo_uri)
+            return client
+        
+        except pymongo.errors.ConnectionFailure as e:
+            print ("Could not connect to server: %s", e)
+            print ("\nTrying to reconnect the server in 5 seconds...")
+            time.sleep(5)
+
+    def Connection_Mysql():
+        try:
+            # Creating connection object
+            mydb = mysql.connector.connect(
+                host = "localhost",
+                user = "root",
+                password = "password@123"
+            )
+            return mydb
+            print("\n _____________ Connected to MYSQL DB _____________________")
+
+        except OperationalError as e:
+            print ("Could not connect to server: %s ", e)
+            print ("\nTrying to reconnect the server in 5 seconds...")
+            time.sleep(5)
+
+
+print(DBConnections.Connection_Mysql())
---
+++ Operations/ops.py
@@ -0,0 +1,89 @@
+from os import path
+import sys
+sys.path.append(path.abspath(r'C:\\Users\\Rizwana\Desktop\Assignments\DAP\DAP_Project'))
+
+from Connections.Connnections_Riz import DBConnections
+import Misc
+import pandas as pd
+import scipy
+import numpy
+import Misc
+import csv 
+
+
+
+file_1 = r"C:\\Users\\Rizwana\Desktop\Assignments\DAP\DAP_Project\Datasets\data01.csv" #health data
+file_2 = r"C:\\Users\\Rizwana\Desktop\Assignments\DAP\DAP_Project\Datasets\\employee-earnings-report-2012.csv" #financial data
+file_3 = r"C:\\Users\\Rizwana\Desktop\Assignments\DAP\DAP_Project\Datasets\\ad_viz_plotval_data (1).csv" #weather data
+
+#Function to insert data in MongoDB database
+def data_dump_mongo(file1, file2, file3):
+    client = DBConnections.Connection_mongo()
+    db = client['DAP_test']
+    collection1 = db.create_collection("Health")
+    collection2 = db.create_collection("Financial")
+    collection3 = db.create_collection("Weather")
+
+    df1 = pd.read_csv(file1, index_col = 0)
+    col_headers1 = list(df1.columns)
+
+    df2 = pd.read_csv(file2, index_col = 0)
+    col_headers2 = list(df2.columns)
+
+    df3 = pd.read_csv(file3, index_col = 0)
+    col_headers3 = list(df3.columns)
+    #print(col_headers)
+
+    with open(file1, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            #print(row)
+            doc={}
+            for n in range(0,len(col_headers1)):
+                doc[col_headers1[n]] = row[n]
+            collection1.insert_one(doc)
+    
+    with open(file2, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            #print(row)
+            doc={}
+            for n in range(0,len(col_headers2)):
+                doc[col_headers2[n]] = row[n]
+            collection2.insert_one(doc)
+    
+    with open(file3, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            #print(row)
+            doc={}
+            for n in range(0,len(col_headers3)):
+                doc[col_headers3[n]] = row[n]
+            collection3.insert_one(doc)
+
+#dumping data in the MONGODB database
+#data_dump_mongo(file_1,file_2,file_3)
+
+
+
+def data_cleaning():
+    print("data_cleaning")
+    client = DBConnections.Connection_mongo()
+    db = client['DAP_test']
+    collection1 = db["Health"]
+    collection2 = db["Financial"]
+    collection3 = db["Weather"]
+
+    health_Data = df = pd.DataFrame(list(collection1.find({})))
+    financial_Data = pd.DataFrame(list(collection2.find({})))
+    weather_Data = pd.DataFrame(list(collection3.find({})))
+
+    print(health_Data.head)
+    print(financial_Data.head)
+    print(weather_Data.head)
+
+    
+
+
+data_cleaning()
+
